fcfb9c33acc2a1e3da1cbd84919e95806fd20f00
yassinehajhaddou <yassinehajhaddou@gmail.com>
2024-06-25 12:09:49 +0200

    Setup Text and Image Preview as Function

--- app/src/main/java/com/app/composestart/ui/BasicPreview.kt
+++ app/src/main/java/com/app/composestart/ui/BasicPreview.kt
@@ -1,13 +1,50 @@
 package com.app.composestart.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.app.composestart.R
+
+@Preview
+@Composable
+fun ImagePreview() {
+    Column {
+        Box(modifier = Modifier.size(100.dp, 80.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.test_image),
+                contentDescription = "Test Image",
+                contentScale = ContentScale.Crop
+            )
+        }
+        Box(modifier = Modifier.size(50.dp, 50.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.test_image),
+                contentDescription = "Test Image",
+                contentScale = ContentScale.Fit
+            )
+        }
+        Box(modifier = Modifier.size(50.dp, 50.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.test_image),
+                contentDescription = "Test Image",
+                contentScale = ContentScale.FillHeight
+            )
+        }
+    }
+}
 
 @Preview
 @Composable
