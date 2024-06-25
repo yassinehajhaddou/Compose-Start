cdd20860f167f8a4c11efecd0e0454de02571165
yassinehajhaddou <yassinehajhaddou@gmail.com>
2024-06-25 12:10:20 +0200

    Implement Text and Image Preview Function

--- app/src/main/java/com/app/composestart/MainActivity.kt
+++ app/src/main/java/com/app/composestart/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.app.composestart.ui.ImagePreview
 import com.app.composestart.ui.TextPreview
 import com.app.composestart.ui.theme.ComposeStartTheme
 
@@ -21,10 +25,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeStartTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    ImagePreview()
+                    Spacer(modifier = Modifier.height(8.dp))
                     TextPreview()
                 }
             }
